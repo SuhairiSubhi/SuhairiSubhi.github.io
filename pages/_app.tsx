fbbe59177e55f428e1cd910fdf4c8e0a292644f2
Suhairi subhi <suhairi.subhi@gmail.com>
2024-06-30 22:36:43 +0800

    feat: update nextjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,10 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Poppins } from "next/font/google";
 
-const poppins = Poppins({ weight: ["100", "200", "400", "700", "900"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "400", "700", "900"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
